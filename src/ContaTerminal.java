76840e9aca31f044ed574d843fc55582dc261d18
Francisco Vinicius <fvrs1@hotmail.com>
2025-04-21 21:45:49 -0300

    Simulando Uma Conta Bancária Através Do Terminal/Console

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,29 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numero;
+        String agencia, nomeCliente;
+        int saldo;
+        
+        // Criação de um objeto Scanner para ler a entrada do usuário
+
+        System.out.println("Por favor, digite o seu nome");
+        Scanner scanner = new Scanner(System.in);
+        nomeCliente = scanner.nextLine();
+        System.out.println("Fico feliz em ter você conosco " + nomeCliente);
+        
+        System.out.println("Por favor " +nomeCliente+ ", digite o número da Agência");
+        agencia = scanner.nextLine();
+
+        System.out.println("Por favor " +nomeCliente+ ", digite o número da Conta");
+        numero = scanner.nextInt();
+        scanner.nextLine();
+
+        Random random = new Random();
+        saldo = random.nextInt(5000);// Gera um saldo aleatório entre 0 e 5000
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta "+numero+ " e seu saldo "+saldo+" já está disponível para saque");
     }
 }
